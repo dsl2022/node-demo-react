bc5f632048533541eaeadf959b3d4f6cbc4c02f5
dsl2022 <2015rpro@gmail.com>
2023-08-14 21:35:20 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import axios from 'axios';
 import './App.css';
-
-async function getData(){
-  let config = {
-    method: 'get',
-    maxBodyLength: Infinity,
-    url: 'localhost:5000/all-pokemons',    
-  };
-  const result = await axios.request(config)
-  return result  
-}
-
+import { useEffect,useState } from 'react';
 
 function App() {
+  const [pokemons,setPokemons]=useState([])
+  useEffect(()=>{
+    function getData(){
+      let config = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: 'http://localhost:5000/all-pokemons',    
+      };
+      try{
+         axios.request(config).then(res=>{
+          setPokemons(res.data.results)
+         })
+        
+      }catch(e){
+        console.log(e)
+      }      
+    }
+    getData()    
+  },[])
+
+  const pokemonsRender = pokemons.map(pokemon=><li>{pokemon.name}</li>)
   return (
     <div className="App">
-    
+      {pokemonsRender}
     </div>
   );
 }
